06e690fce7a03d0b39951018c329f697517e8a2e
moeamaya <moeamaya@gmail.com>
2015-04-27 11:12:25 -0700

    removed position fixed on body and used height:100% instead

--- source/assets/javascripts/hydrogen-0.0.1.js
+++ source/assets/javascripts/hydrogen-0.0.1.js
@@ -13,6 +13,7 @@ if ('addEventListener' in document) {
 
 	var body = document.body,
 		  main = document.querySelector( '#main' ),
+		  htmlElement = document.querySelector("html"),
 		  openbtn = document.getElementById( 'nav-open-button' ),
 		  closebtn = document.getElementById( 'nav-close-button' ),
 		  isOpen = false;
@@ -45,12 +46,14 @@ if ('addEventListener' in document) {
 			classie.remove( nav, 'nav-open');
 			classie.add( nav, 'nav-right');
 			classie.remove( body, 'nav-active' );
+			classie.remove(htmlElement, 'nav-active');
 			classie.remove( main, 'main-inactive');
 		}
 		else {
 			classie.add( nav, 'nav-open');
 			classie.remove( nav, 'nav-right');
 			classie.add( body, 'nav-active' );
+			classie.add(htmlElement, 'nav-active');
 			classie.add( main, 'main-inactive');
 		}
 		isOpen = !isOpen;
