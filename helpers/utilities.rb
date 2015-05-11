75df393d42209562b0242e4b18096e0a2db12b02
Alexander M Dixon <alexander.m.dixon@gmail.com>
2015-05-11 10:14:32 -0700

    big push

--- helpers/utilities.rb
+++ helpers/utilities.rb
@@ -11,12 +11,52 @@ module Utilities
     # Set the url
     url = args[url_idx]
 
+    # Get the parent url
+    unless page == "/" || page == ""
+      parent_url = "/" + page.to_s.split("/")[1] + "/"
+    end
+
     options = args[options_idx] || {}
     options[:class] ||= ""
 
     if url == page
       options[:class] << " active"
       args[options_idx] = options
+    elsif parent_url == "/arrows/"
+      if url == parent_url
+        options[:class] << " active"
+        args[options_idx] = options
+      end
+    elsif parent_url == "/currency/"
+      if url == parent_url
+        options[:class] << " active"
+        args[options_idx] = options
+      end
+    elsif parent_url == "/letters/"
+      if url == parent_url
+        options[:class] << " active"
+        args[options_idx] = options
+      end
+    elsif parent_url == "/math/"
+      if url == parent_url
+        options[:class] << " active"
+        args[options_idx] = options
+      end
+    elsif parent_url == "/numbers/"
+      if url == parent_url
+        options[:class] << " active"
+        args[options_idx] = options
+      end
+    elsif parent_url == "/punctuation/"
+      if url == parent_url
+        options[:class] << " active"
+        args[options_idx] = options
+      end
+    elsif parent_url == "/symbols/"
+      if url == parent_url
+        options[:class] << " active"
+        args[options_idx] = options
+      end
     end
 
     link_to(*args, &block)
