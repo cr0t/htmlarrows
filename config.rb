c06659736d1c68c7b805f840cac307148ef4a5ce
Alexander M Dixon <alexander.m.dixon@gmail.com>
2015-09-11 11:54:10 -0700

    removed autoprefixer

--- config.rb
+++ config.rb
@@ -47,11 +47,6 @@ page "/sitemap.xml", layout: false
 # Pretty URLs
 activate :directory_indexes
 
-# Enable autoprefixing on CSS
-activate :autoprefixer do |config|
-  config.ignore   = ['normalize.scss']
-end
-
 # Define asset paths
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
